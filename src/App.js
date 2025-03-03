639166d5dafaf3235536e1add458c31114d9c3f1
aap9002 <aap9002@gmail.com>
2025-03-03 12:36:19 +0000

    add basic site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <div className="dynamic-background"></div>
+      <header className="header">
+        <h1>Green Leaf Group LTD</h1>
+        <p>Affordable Housing | Sustainable Future</p>
       </header>
+      <main className="content">
+
+        <div className="mission-callout">
+          <h2>Our mission</h2>
+          <hr/>
+          <p>
+            Provide affordable, quality homes while fostering a greener future.
+          </p>
+          <hr/>
+        </div>
+
+        <section className="business-details">
+          <h1>Contact us</h1>
+          <hr/>
+          <div className="contact-details">
+            <a href="mailto:admin@glglettings.com">admin@glglettings.com</a>
+          </div>
+          <div className="contact-details">
+            <a href="tel:+447957842167">+44 7957 842167</a>
+          </div>
+          <button
+            className="call-button"
+            onClick={() => window.location.href = 'tel:+447957842167'}
+          >
+            Call Our Office
+          </button>
+        </section>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
